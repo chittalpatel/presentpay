98eb94f36318496c64dbfc467769c754a8d651e2
chittal <email.chittal@gmail.com>
2023-01-19 23:17:07 +0530

    settings for prod

--- presentpay/settings.py
+++ presentpay/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^on25!30ld@-szv+4_nexkt2_1qn*k0-8kub6fyn+5g9_$ep9$"
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "django-insecure-^on25!30ld@-szv+4_nexkt2_1qn*k0-8kub6fyn+5g9_$ep9$"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", ".amazonaws.com"]
 
 # Application definition
 
@@ -46,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -137,3 +142,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 FILTERS_EMPTY_CHOICE_LABEL = "All"
 PHONENUMBER_DEFAULT_REGION = "IN"
+
+# https://pypi.org/project/whitenoise/
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+USE_X_FORWARDED_PORT = True
